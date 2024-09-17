01ba5039940020f38eb774de7e036cc494210e0a
Andrew Kuznetsov <56806222+endrewsmith@users.noreply.github.com>
2024-09-17 17:13:29 +0300

    Update Node.java

--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -15,11 +15,14 @@ import lombok.Setter;
 public class Node<T> {
 
     private T value;
-
     private Node<T> next;
+    // Добавляем размер
+    private int size = 0;
 
     public Node(T value) {
         this.value = value;
+        // Увеличиваем размер на 1
+        size++;
     }
 
     public Node<T> add(T value) {
@@ -29,6 +32,26 @@ public class Node<T> {
         }
         Node<T> newNode = new Node<>(value);
         emptyNode.setNext(newNode);
+        // Увеличиваем размер на 1
+        size++;
         return newNode;
     }
+
+    // Добавляем метод получения элемента по индексу для тестов
+    public T get(int index){
+        if (index >= size || index < 0) {
+            throw new IllegalArgumentException("invalid index");
+        }
+        Node<T> curr = this;
+        for (int i = 0; i < index; i++) {
+            curr = curr.next;
+        }
+        return curr.value;
+    }
+
+    // Добавляем получение размера
+    public int size() {
+        return size;
+    }
 }
+
