85ac6c2c38de9ef22bd6c9eda5953dd9a432dfb0
Endrew Smith <assmith@mail.ru>
2024-09-19 00:21:49 +0300

    edit tests

--- src/test/java/org/example/HomeWorkTest.java
+++ src/test/java/org/example/HomeWorkTest.java
@@ -1,12 +1,11 @@
 package org.example;
 
-import org.example.HomeWork;
-import org.example.Node;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.Random;
 import java.util.function.Predicate;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class HomeWorkTest {
 
@@ -18,21 +17,21 @@ public class HomeWorkTest {
 
         Node<Integer> node = new Node<>(0);
         // Проверка get на отрицальный индекс
-        Assert.assertThrows(IllegalArgumentException.class, ()-> node.get(-10));
+        assertThrows(IllegalArgumentException.class, () -> node.get(-10));
 
-        Assert.assertEquals(0, node.get(0));
-        for(int i = 1; i < sizeNode; i++){
+        assertEquals(0, node.get(0));
+        for (int i = 1; i < sizeNode; i++) {
             node.add(i);
-            Assert.assertEquals(i, node.get(i));
+            assertEquals(i, node.get(i));
         }
         // Проверка size() на исключение
-        Assert.assertEquals(sizeNode, node.size());
+        assertEquals(sizeNode, node.size());
         // Проверка get() на исключение
-        Assert.assertThrows(IllegalArgumentException.class, ()-> node.get(1001));
+        assertThrows(IllegalArgumentException.class, () -> node.get(1001));
 
         // Проверка get на рандомное значение индекса
         Integer random = new Random().nextInt(sizeNode);
-        Assert.assertEquals(random, node.get(random));
+        assertEquals(random, node.get(random));
     }
 
     // Тест метода partitionBy(Node<T> list, Predicate<T> pred)
@@ -45,28 +44,28 @@ public class HomeWorkTest {
         Predicate<Integer> pred_first = x -> x < 4;
         // Создаем список с последовтельностью: 1, 2, 3, 4, 5
         Node<Integer> node = new Node<>(1);
-        Assert.assertEquals(1, node.get(0));
-        for(int i = 2; i < 6; i++){
+        assertEquals(1, node.get(0));
+        for (int i = 2; i < 6; i++) {
             node.add(i);
-            Assert.assertEquals(i, node.get(i-1));
+            assertEquals(i, node.get(i - 1));
         }
 
         // Проверки на null, при наличие null в параметрах метод возвращает 0
-        Assert.assertEquals(0, homeWork.partitionBy(node, null));
-        Assert.assertEquals(0, homeWork.partitionBy(null, null));
-        Assert.assertEquals(0, homeWork.partitionBy(null, pred_first));
+        assertEquals(0, homeWork.partitionBy(node, null));
+        assertEquals(0, homeWork.partitionBy(null, null));
+        assertEquals(0, homeWork.partitionBy(null, pred_first));
 
         // Проверка на первое условие
-        Assert.assertEquals(3, homeWork.partitionBy(node, pred_first));
+        assertEquals(3, homeWork.partitionBy(node, pred_first));
 
         // Второе условие
         Predicate<Integer> pred_second = x -> x < 0;
         // Проверка на второе условие
-        Assert.assertEquals(0, homeWork.partitionBy(node, pred_second));
+        assertEquals(0, homeWork.partitionBy(node, pred_second));
 
         // В методе add(T value) нет ограничения на value
         node.add(null);
-        Assert.assertEquals(null, node.get(5));
+        assertEquals(null, node.get(5));
     }
 
     // Тест метода findNthElement(Node<T> list, int n)
@@ -77,17 +76,17 @@ public class HomeWorkTest {
         HomeWork homeWork = new HomeWork();
         // Создаем список для наглядности начнем с 0
         Node<Integer> node = new Node<>(0);
-        for(int i = 1; i < sizeNode; i++){
+        for (int i = 1; i < sizeNode; i++) {
             node.add(i);
-            Assert.assertEquals(i, node.get(i));
+            assertEquals(i, node.get(i));
         }
 
         // Тест на поиск по отрицательному значению
-        Assert.assertThrows(IllegalArgumentException.class, ()-> homeWork.findNthElement(node,-1));
+        assertThrows(IllegalArgumentException.class, () -> homeWork.findNthElement(node, -1));
 
         // Тест на поиск по рандомному значению
         Integer random = new Random().nextInt(sizeNode);
-        Assert.assertEquals(random, homeWork.findNthElement(node, random));
+        assertEquals(random, homeWork.findNthElement(node, random));
     }
 
     // Тесты к этому методу совпадают с тестами метода get
@@ -96,12 +95,12 @@ public class HomeWorkTest {
         HomeWork homeWork = new HomeWork();
         // Создаем список для наглядности начнем с 0
         Node<Integer> node = new Node<>(0);
-        for(int i = 1; i < 100; i++){
+        for (int i = 1; i < 100; i++) {
             node.add(i);
-            Assert.assertEquals(i, node.get(i));
+            assertEquals(i, node.get(i));
         }
         // Проверим, что два метода работают одинаково
         Integer random = new Random().nextInt(100);
-        Assert.assertEquals(homeWork.findNthElementNodeGet(node, random), homeWork.findNthElement(node, random));
+        assertEquals(homeWork.findNthElementNodeGet(node, random), homeWork.findNthElement(node, random));
     }
 }
